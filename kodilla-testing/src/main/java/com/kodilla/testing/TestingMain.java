d8577e4186481433632ef72824aef9a65286a4ee
Smagii <smagpaw@gmail.com>
2020-02-05 21:34:48 +0100

    5th commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+import com.sun.javaws.IconUtil;
+
+import java.sql.SQLOutput;
+
+import static com.kodilla.testing.calculator.Calculator.*;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result= simpleUser.getUserName();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int resultCalculator= calculator.add(4,4);
+
+        if (resultCalculator==a+b){
+            System.out.println("test OK");
+        }else{
+            System.out.println("error!");
+        }
+
     }
 }
